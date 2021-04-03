40ca1870df21aa53110318d8c8de96e052f6cef3
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-04-03 21:47:24 +0200

    add time and space

--- java/junit5/src/main/java/leetcode/AreSentencesSimilar.java
+++ java/junit5/src/main/java/leetcode/AreSentencesSimilar.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
  * @author YUQIU
  */
 public class AreSentencesSimilar { // https://leetcode.com/problems/sentence-similarity-iii/
-	boolean areSentencesSimilar(String sentence1, String sentence2) {
+	boolean areSentencesSimilar(String sentence1, String sentence2) { // time: O(s1 + s2), space: O(s1 + s2)
 		if (sentence1.equals(sentence2) || sentence1.isEmpty() || sentence2.isEmpty()) return true;
 		String[] sentence1Split = sentence1.split(" ");
 		String[] sentence2Split = sentence2.split(" ");
@@ -18,11 +18,11 @@ public class AreSentencesSimilar { // https://leetcode.com/problems/sentence-sim
 		LinkedList<String> queue2 = new LinkedList<>();
 		queue2.addAll(Arrays.asList(sentence2Split));
 
-		while (notEmpty(queue1, queue2) && queue1.getFirst().equals(queue2.getFirst())) {
+		while (bothNotEmpty(queue1, queue2) && queue1.getFirst().equals(queue2.getFirst())) {
 			queue1.removeFirst();
 			queue2.removeFirst();
 		}
-		while (notEmpty(queue1, queue2) && queue1.getLast().equals(queue2.getLast())) {
+		while (bothNotEmpty(queue1, queue2) && queue1.getLast().equals(queue2.getLast())) {
 			queue1.removeLast();
 			queue2.removeLast();
 		}
@@ -31,7 +31,7 @@ public class AreSentencesSimilar { // https://leetcode.com/problems/sentence-sim
 		return false;
 	}
 
-	private boolean notEmpty(LinkedList<String> queue1, LinkedList<String> queue2) {
+	private boolean bothNotEmpty(LinkedList<String> queue1, LinkedList<String> queue2) {
 		return !queue1.isEmpty() && !queue2.isEmpty();
 	}
 }
