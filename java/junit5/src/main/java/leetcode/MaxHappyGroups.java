72e92d74c43fb784eec8f637c7a979f8cb1ae31c
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-04-04 12:01:38 +0200

    [1815] * refactoring * add dp

--- java/junit5/src/main/java/leetcode/MaxHappyGroups.java
+++ java/junit5/src/main/java/leetcode/MaxHappyGroups.java
@@ -1,38 +1,46 @@
 package leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 // understand the recursive solution here https://github.com/geyuqiu/kata-bootstraps/blob/master/java/junit5/src/main/java/crackingTheCodingInterview/listOfStringPermutations.java first :)
 public class MaxHappyGroups { // https://leetcode.com/problems/maximum-number-of-groups-getting-fresh-donuts/
-    Map<String, Integer> modRemainders = new HashMap<>();
+    Map<String, Integer> modRemaindersCounter = new HashMap<>();
 
-	int maxHappyGroups(int batchSize, int[] groups) { //
-        int[] remainders = new int[batchSize];
+	int maxHappyGroups(int batchSize, int[] groups) { // time and space: O((G/K)^K*K), B = K/2
+        int[] remaindersCounter = new int[batchSize];
         for (int numOfCustomer : groups) {
-            remainders[numOfCustomer % batchSize]++;
+            remaindersCounter[numOfCustomer % batchSize]++;
         }
-        int happyGroups = remainders[0];
-        remainders[0] = 0; // start dfs from 1
-        int ans = remaindersPermutations(0, remainders);
+        int happyGroups = remaindersCounter[0];
+        remaindersCounter[0] = 0; // start dfs from 1
+        int ans = remaindersPermutations(0, remaindersCounter);
 
         return ans + happyGroups;
     }
 
-    int remaindersPermutations(int mod, int[] remainders) {
-		if (checkBaseCase(remainders)) return 0;
+    int remaindersPermutations(int mod, int[] remaindersCounter) {
+		if (checkBaseCase(remaindersCounter)) return 0;
+
+		String key = mod +"," + Arrays.toString(remaindersCounter);
+		if (modRemaindersCounter.containsKey(key)) {
+			return modRemaindersCounter.get(key);
+		}
 
         int firstGroupGetsFreshDonuts = (mod == 0) ? 1 : 0;
         int max = 0;
-        for (int i = 1; i < remainders.length; i++) {
-            if (remainders[i] > 0) {
-                remainders[i]--;
-                max = Math.max(max, remaindersPermutations((mod + i) % remainders.length, remainders));
-                remainders[i]++;
+        for (int i = 1; i < remaindersCounter.length; i++) {
+            if (remaindersCounter[i] > 0) {
+                remaindersCounter[i]--;
+                max = Math.max(max, remaindersPermutations((mod + i) % remaindersCounter.length, remaindersCounter));
+                remaindersCounter[i]++;
             }
         }
 
-        return max + firstGroupGetsFreshDonuts;
+        mod = max + firstGroupGetsFreshDonuts;
+		modRemaindersCounter.put(key, mod);
+		return mod;
     }
 
 	private boolean checkBaseCase(int[] remainders) {
