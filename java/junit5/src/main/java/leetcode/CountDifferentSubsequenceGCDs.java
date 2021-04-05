b4d16f9bffcd48a930035df4c35352becacab341
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-04-05 11:55:53 +0200

    [1819] * refactoring

--- java/junit5/src/main/java/leetcode/CountDifferentSubsequenceGCDs.java
+++ java/junit5/src/main/java/leetcode/CountDifferentSubsequenceGCDs.java
@@ -9,36 +9,26 @@ public class CountDifferentSubsequenceGCDs { // https://leetcode.com/problems/nu
 	int countDifferentSubsequenceGCDs(int[] nums) { // time: O( M * log(M) * log(M)), space : O(M), M: max of nums[i]
 
 		int max = 0;
-		for (int n: nums) {
-			max = Math.max(n, max);
-		}
+		for (int n: nums) max = Math.max(n, max);
 
 		int len = max + 1;
 		boolean[] exists = new boolean[len];
-		for (int n : nums) {
-			exists[n] = true;
-		}
+		for (int n : nums) exists[n] = true;
 
 		int gcdCount = 0;
-		for (int i = 1; i < len; i++) { // 0 not treated
-			boolean found = checkGcdIFold(exists, i);
-			if (found) gcdCount++;
-		}
+		for (int i = 1; i < len; i++)
+			if (subsequenceFound(exists, i))
+				gcdCount++;
+
 		return gcdCount;
 	}
 
-	boolean checkGcdIFold(boolean[] exists, int gcd) {
-		//gcd(0, X) -> X
-		int calculatedGcd = 0;
+	boolean subsequenceFound(boolean[] exists, int factor) {
+		int calculatedGcd = 0; //gcd(0, X) -> X
 
-		for (int i = gcd; i < exists.length; i += gcd) {
-			if (exists[i]) {
-				calculatedGcd = gcd(calculatedGcd, i); // gcd(2*3,2*5) = 2
-			}
-			// gcd(4,16) = 4 != 2
-			if (calculatedGcd == gcd) {
-				return true;
-			}
+		for (int i = factor; i < exists.length; i += factor) {
+			if (exists[i]) calculatedGcd = gcd(calculatedGcd, i); // gcd(2*3,2*5) = 2
+			if (calculatedGcd == factor) return true; // gcd(4,16) = 4 != 2
 		}
 		return false;
 	}
