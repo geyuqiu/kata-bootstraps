6c6aa2c49b620beec1482d651ef4ecd0312da678
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-04-05 04:01:56 +0200

    [1819] * use boolean array * reevaluated gcd(4,16) != 2 * O( n * log(n) * log(n))

--- java/junit5/src/main/java/leetcode/CountDifferentSubsequenceGCDs.java
+++ java/junit5/src/main/java/leetcode/CountDifferentSubsequenceGCDs.java
@@ -1,67 +1,44 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 /**
  * @author YUQIU
  */
 public class CountDifferentSubsequenceGCDs { // https://leetcode.com/problems/number-of-different-subsequences-gcds/
 	// 1 <= nums.length <= 10^5
 	// 1 <= nums[i] <= 2 * 10^5
-	int countDifferentSubsequenceGCDs(int[] nums) { // time: O( 10000 * n * log(n)), space : O(n)
-
-		Set<Integer> gcds = new HashSet<>();
+	int countDifferentSubsequenceGCDs(int[] nums) { // time: O( n * log(n) * log(n)), space : O(n)
 
 		int max = 0;
 		for (int n: nums) {
 			max = Math.max(n, max);
-			gcds.add(n);
-		}
-		max = Math.min(max, 100000);
-
-		for (int i = 0; i < max; i++) { // 0 -> % 1,  max-1 -> % max,
-			int gcd = i + 1;
-			if (!gcds.contains(gcd)) {
-				if (checkIfAtLeast2DivisableBy(gcd, nums)) {
-					gcds.add(gcd);
-				}
-			}
 		}
 
-		return gcds.size();
+		int len = max + 1;
+		boolean[] exists = new boolean[len];
+		for (int n : nums) {
+			exists[n] = true;
+		}
+
+		int gcdCount = 0;
+		for (int i = 1; i < len; i++) { // 0 not treated
+			boolean found = checkGcdIFold(exists, i);
+			if (found) gcdCount++;
+		}
+		return gcdCount;
 	}
 
-	private boolean checkIfAtLeast2DivisableBy(int gcd, int[] nums) {
-		List<Integer> gcds = new ArrayList<>();
-		int counter = 0;
-		for (int n: nums) {
-			if (n % gcd == 0) {
-				counter++;
-				gcds.add(n);
-			}
-		}
+	boolean checkGcdIFold(boolean[] exists, int gcd) {
+		//gcd(0, X) -> X
+		int calculatedGcd = 0;
 
-		if (counter < 2) return false;
-		ArrayList<Integer> divided = new ArrayList<>();
-		for (int n : gcds) {
-			divided.add(n/gcd);
+		for (int i = gcd; i < exists.length; i += gcd) {
+			if (exists[i]) {
+				calculatedGcd = gcd(calculatedGcd, i);
+			}
+			// gcd(4,16) = 4 != 2
+			if (calculatedGcd == gcd) return true;
 		}
-		if (gcd(divided) == 1) return true; // if you divide each number in the subsequence by x, then the gcd of the resulting numbers should be 1.
 		return false;
-	}
-
-	int gcd(List<Integer> nums) { // O(n * log(s)), s being the smallest number of nums
-		if (nums.contains(1)) return 1;
-		int result = nums.get(0); // associativity: gcd(a,b,c,d) = gcd(gcd(gcd(a,b),c),d)
-		for (int i = 1; i < nums.size(); i++) {
-			result = gcd(result, nums.get(i));
-
-			if (result == 1) return 1;
-		}
-		return result;
 	}
 
 	int gcd(int a, int b) { // O(log(a+b))
