cceb499f4a1752fdf78fa5ac12f39a96e903735e
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-04-12 17:59:00 +0200

    [1825]  BST in sortedSlidingWindow

--- java/junit5/src/main/java/leetcode/MKAverage.java
+++ java/junit5/src/main/java/leetcode/MKAverage.java
@@ -7,58 +7,45 @@ import java.util.*;
  * n: 10^5
  * m: 10^5
  * k: 10^5 / 2
- * similar to this: https://leetcode.com/problems/sliding-window-maximum/
+ * save sliding window elements as sorted list as BST array ?
+ * similar to this: https://leetcode.com/problems/sliding-window-maximum/, how do I persist the rest of k-2 elements ?
  */
 class MKAverage { // space: O(n), time: O(n) but for sorting alone: O(n * mlogm), n: # of calculateMKAverage operations
 
 	int m;
 	int k;
-	List<Integer> intList;
-	Queue<Integer> queue;
+	List<Integer> nums;
+	List<Integer> sortedSlidingWindow;
 
 	public MKAverage(int m, int k) {
 		this.m = m;
 		this.k = k;
-		this.intList = new ArrayList<>();
-		this.queue = new PriorityQueue<>(m);
+		nums = new ArrayList<>();
 	}
 
 	public void addElement(int num) { // how to keep sorting ?
-		this.intList.add(num);
+		nums.add(num);
 	}
 
-	public int calculateMKAverage() {
-		int n = intList.size();
-		if (n < m) return -1;
-
-		for (int i = n - 1; i >= n - m; i--) {  // as priority queue but still have bottleneck for insertion: O(n*mlogm), so same thing
-			queue.add(this.intList.get(i));
-		}
-
-//		System.out.println(intList);
-//		System.out.println(queue);
-		return averageInQueue(queue, k, queue.size() - k - 1); // m=6, k=1 -> average([1, 3, 3, 4, 5, 12], 1, 4), #nums = 4-1+1
-	}
-
-	int averageInQueue(Queue<Integer> indexQueue, int from, int to) { // O(k)
+	int averageInQueue(Queue<Integer> indexQueue) { // O(k)
 		long sum = 0L;
 		int i = 0;
 		while (!indexQueue.isEmpty()) {
 			int num = indexQueue.poll();
-			if (i >= from && i <= to) {
+			if (i >= k && i <= indexQueue.size() - k - 1) {
 				sum += num;
 			}
 			i++;
 		}
 //		 System.out.println(sum);
 
-		return Math.round(sum / (to - from + 1));
+		return Math.round(sum / (m - 2 * k));
 	}
 
-	int averageInList(List<Integer> l, int from, int to) {
+	int averageInList(List<Integer> l) {
 		long sum = 0L;
-		for (int i = from; i <= to; i++) sum += (long) l.get(i);
+		for (int i = k; i <= l.size() - k - 1; i++) sum += (long) l.get(i);
 
-		return Math.round(sum / (to - from + 1));
+		return Math.round(sum / (m - 2 * k));
 	}
 }
