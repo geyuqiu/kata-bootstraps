4b82ea3574cea38c966ba304847a70ad182c1817
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-05-01 21:07:34 +0200

    [1846] add priority queue

--- python/unittest/seatManager.py
+++ python/unittest/seatManager.py
@@ -1,19 +1,19 @@
-class SeatManager:
+from queue import PriorityQueue
+
+class SeatManager:  # TLE O(C*N) -> O(C * log n)
     def __init__(self, n: int):
         self.r = [False] * n
         self.N = n
+        self.q = PriorityQueue()
+        for i in range(0, n):
+            self.q.put(i)
 
     def reserve(self) -> int:
-        index = self.avail()
+        index = self.q.get()
         self.r[index] = True
 
         return index + 1
 
     def unreserve(self, seatNumber: int) -> None:
         self.r[seatNumber - 1] = False
-
-    def avail(self) -> int:
-        for i in range(0, self.N):
-            if (not self.r[i]):
-                return i
-        return -1
+        self.q.put(seatNumber - 1)  # O(log n)
