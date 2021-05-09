b6701270327d3ce79cf27b2123040c9c3bbd69ea
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-05-09 17:05:36 +0200

    todo: failing edge case

--- java/junit5/src/main/java/leetcode/MaxDistance.java
+++ java/junit5/src/main/java/leetcode/MaxDistance.java
@@ -6,15 +6,14 @@ import java.util.Arrays;
  * @author YUQIU
  */
 public class MaxDistance {
-	int maxDistance(int[] nums1, int[] nums2) { // O(n1 * n2)
+	int maxDistance(int[] nums1, int[] nums2) { // O(n * logn)
 		int max = 0;
 		for (int i = 0; i < nums1.length; i++) {
-//			for (int j = i; j < nums2.length; j++) {
-//				if (nums1[i] <= nums2[j]) max = Math.max(j - i, max);
-//				else break; // non-increasing
-//			}
 			int index = binarySearch(nums2, 0, nums2.length, nums1[i]);
 			System.out.println( nums1[i] + ": " + index);
+			if (index > i) {
+				max = Math.max(index - i, max);
+			}
 		}
 
 		return max;
@@ -30,12 +29,12 @@ public class MaxDistance {
 			int midVal = a[mid];
 
 			if (midVal < key)
-				low = mid + 1;
-			else if (midVal > key)
 				high = mid - 1;
+			else if (midVal > key)
+				low = mid + 1;
 			else
 				return mid; // key found
 		}
-		return low;  // key not found.
+		return low-1;  // key not found.
 	}
 }
--- java/junit5/src/test/java/leetcode/MaxDistanceTest.java
+++ java/junit5/src/test/java/leetcode/MaxDistanceTest.java
@@ -19,7 +19,10 @@ class MaxDistanceTest {
 	private static Stream<Arguments>
 	maxDistance() {
 		return Stream.of(
-			Arguments.of(new int[]{55, 30, 5, 4, 2},new int[]{100, 20, 10, 10, 5}, 2)
+			Arguments.of(new int[]{55, 30, 5, 4, 2},new int[]{100, 20, 10, 10, 5}, 2),
+			Arguments.of(new int[]{2, 2, 2},new int[]{10, 10, 1}, 1),
+			Arguments.of(new int[]{30, 29, 19, 5},new int[]{25, 25, 25, 25, 25}, 2),
+			Arguments.of(new int[]{5,4},new int[]{3,2}, 0)
 		);
 	}
 
