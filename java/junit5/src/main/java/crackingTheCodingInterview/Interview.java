051dec30b2c8d147f5d285cce4a39e713781344e
ge-yuqiu <yuqiu.ge@gmail.com>
2021-05-13 23:09:18 +0200

    sumOfContiguousFragments

--- java/junit5/src/main/java/crackingTheCodingInterview/Interview.java
+++ java/junit5/src/main/java/crackingTheCodingInterview/Interview.java
@@ -4,74 +4,24 @@ package crackingTheCodingInterview;
  *
  */
 public class Interview {
-	int solution(int[] A) { // O(n * n)
-		int leftAndRight = leftAndRight(A);
-		int onlyLeft = onlyLeft(A);
-		int onlyRight = onlyRight(A);
+	int solution(int[] A, int S) { // O(n ^ 3)
+		int sumOfContiguousFragments = 0;
 
-		return Math.min(leftAndRight, Math.min(onlyLeft, onlyRight));
-	}
-
-	private int leftAndRight(int[] a) {
-		int i;
-		int j;
-		boolean leftBoundarySet = false;
-		boolean rightBoundarySet = false;
-
-		for (i = 0, j = a.length-1; i < a.length && j>i;) {
-			if (leftBoundarySet && rightBoundarySet) break;
-			int li = getIndex(a, i+1, j, a[i]);
-			if (li >= 0) {
-				i++;
-			} else {
-				leftBoundarySet = true;
-			}
-			int ri = getIndex(a, i, j-1, a[j]);
-			if (ri >= 0) {
-				j--;
-			} else {
-				rightBoundarySet = true;
+		for (int i = 0; i < A.length; i++) {
+			// j-i is the number of elements which should be in the fragment
+			for (int j = i; j < A.length; j++) {
+				int sumOfFragments = 0;
+				int counter = j-i+1;
+				for (int k = i; k <= j; k++) {
+//					System.out.print(A[k]);
+					sumOfFragments += A[k];
+				}
+				double mean = (double) sumOfFragments / counter;
+				if (mean == Double.valueOf(S)) sumOfContiguousFragments++;
+//				System.out.println();
 			}
 		}
 
-		int leftAndRight = j - i + 1;
-		return leftAndRight;
-	}
-
-	private int onlyLeft(int[] a) {
-		int i;
-
-		for (i = 0; i < a.length;) {
-			int li = getIndex(a, i+1, a.length - 1, a[i]);
-			if (li >= 0) {
-				i++;
-			} else {
-				break;
-			}
-		}
-
-		return a.length - i;
-	}
-
-	private int onlyRight(int[] a) {
-		int i;
-
-		for (i = a.length-1; i >= 0;) {
-			int li = getIndex(a, 0, i-1, a[i]);
-			if (li >= 0) {
-				i--;
-			} else {
-				break;
-			}
-		}
-
-		return i + 1;
-	}
-
-	private int getIndex(int[] a, int i, int j, int key) {
-		for (int k = i; k <=j ; k++) {
-			if (key == a[k]) return k;
-		}
-		return -1;
+		return sumOfContiguousFragments;
 	}
 }
--- java/junit5/src/test/java/crackingTheCodingInterview/InterviewTest.java
+++ java/junit5/src/test/java/crackingTheCodingInterview/InterviewTest.java
@@ -18,19 +18,16 @@ class InterviewTest {
 	private static Stream<Arguments>
 	test() {
 		return Stream.of(
-			Arguments.of(new int[]{1,2,3,4,5,4, 1,2,3,4}, 5),
-			Arguments.of(new int[]{7, 3, 7, 3, 1, 3, 4, 1}, 5),
-			Arguments.of(new int[]{2, 1, 1, 3, 2, 1, 1, 3}, 3),
-			Arguments.of(new int[]{7, 5, 2, 7, 2, 7, 4, 7}, 6),
-			Arguments.of(new int[]{7,2,3}, 3),
-			Arguments.of(new int[]{1,2,3,4,5,4}, 5)
+			Arguments.of(new int[]{2,1,3}, 2, 3),
+			Arguments.of(new int[]{0,4,3,-1}, 2, 2),
+			Arguments.of(new int[]{2,1,4}, 3, 0)
 		);
 	}
 	@ParameterizedTest
 	@MethodSource
 	void
-	test(int[] A, int expected) {
-		int actual = interview.solution(A);
+	test(int[] A, int S, int expected) {
+		int actual = interview.solution(A, S);
 
 		assertThat(actual).isEqualTo(expected);
 	}
