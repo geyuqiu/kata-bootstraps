8c91672f443a22b29dc431cf8092fb4c1b1f2e59
ge-yuqiu <yuqiu.ge@gmail.com>
2021-05-13 22:10:46 +0200

    2. binarySearch only works for sorted array

--- java/junit5/src/main/java/crackingTheCodingInterview/Interview.java
+++ java/junit5/src/main/java/crackingTheCodingInterview/Interview.java
@@ -1,10 +1,35 @@
 package crackingTheCodingInterview;
 
+import java.util.Arrays;
+
 /**
  *
  */
 public class Interview {
-	int solution(int[] A) {
-		return 1;
+	int solution(int[] A) { // binary search: O(logn * n)
+		int i;
+		int j;
+		boolean leftBoundarySet = false;
+		boolean rightBoundarySet = false;
+
+		for (i = 0, j = A.length-1; i < A.length && j>i;) {
+			if (leftBoundarySet && rightBoundarySet) break;
+			int li = Arrays.binarySearch(A, i, j+1, A[i]);
+			if (li >= 0) {
+				i++;
+			} else {
+				leftBoundarySet = true;
+			}
+			int ri = Arrays.binarySearch(A, i, j+1, A[j]);
+			if (ri >= 0) {
+				j--;
+			} else {
+				rightBoundarySet = true;
+			}
+		}
+
+		return j-i + 1;
 	}
+
+
 }
