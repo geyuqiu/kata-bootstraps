e8f5a23f25609cc5e4ca311cc57b62f1f999c6f7
ge-yuqiu <yuqiu.ge@gmail.com>
2021-05-29 19:59:03 +0200

    [5757]: add findBetween

--- java/junit5/src/main/java/leetcode/GetBiggestThree.java
+++ java/junit5/src/main/java/leetcode/GetBiggestThree.java
@@ -7,15 +7,16 @@ import java.util.*;
  */
 public class GetBiggestThree { // O(m*n)
 	List<Integer> r = new ArrayList<>();
+
 	int[] getBiggestThree(int[][] grid) {
 		int m = grid.length;
 		int n = grid[0].length;
 		findDiameter1x1(grid, m, n);
 
 		int index = 0;
-		if ( m >= 3 && n >= 3) {
+		if (m >= 3 && n >= 3) {
 			int[] diameters = new int[49]; // m, n <= 100
-			for (int i = 3; i < 100; i+=2, index++) {
+			for (int i = 3; i < 100; i += 2, index++) {
 				diameters[index] = i;
 			}
 			for (int i = 0; i < 49; i++) {
@@ -27,19 +28,19 @@ public class GetBiggestThree { // O(m*n)
 		// todo: replace with max 3 items without duplicates
 		Collections.sort(r, Collections.reverseOrder());
 		LinkedHashMap<Integer, Boolean> hm = new LinkedHashMap<>();
-		for (int i: r) {
+		for (int i : r) {
 			if (!hm.containsKey(i)) {
 				hm.put(i, true);
 			}
 		}
 
 		int len;
-		if (hm.size() < 3)  len = hm.size();
+		if (hm.size() < 3) len = hm.size();
 		else len = 3;
 		int[] res = new int[len];
 		index = 0;
 
-		for (Map.Entry<Integer, Boolean> entry: hm.entrySet()) {
+		for (Map.Entry<Integer, Boolean> entry : hm.entrySet()) {
 			res[index] = entry.getKey();
 			index++;
 			if (index == 3) break;
@@ -58,23 +59,23 @@ public class GetBiggestThree { // O(m*n)
 	}
 
 	private void findDiameter(int[][] grid, int m, int n, int diameter) { // each of the corners centered in a grid cell, 7 = 7/2 + 1 + 7/2
-		int sum = 0;
 
-		for (int i = m / 2; i <= m-1-m/2; i++) { // grid[i][j] as the center of Rhombus
-			for (int j = n/2; j <= n -1-n/2; j++) {
+		for (int i = m / 2; i <= m - 1 - m / 2; i++) { // grid[i][j] as the center of Rhombus
+			for (int j = n / 2; j <= n - 1 - n / 2; j++) {
+				int sum = 0;
 
 				// similar to css margin: 1px 2px 3px 4px
-				int bottomX = i + m / 2;
-				int bottomY = j;
-				int bottom = grid[bottomX][bottomY];
-
 				int topX = i - m / 2;
 				int topY = j;
 				int top = grid[topX][topY];
 
-				int rightY = j + n / 2;
 				int rightX = i;
+				int rightY = j + n / 2;
 				int right = grid[rightX][rightY];
+
+				int bottomX = i + m / 2;
+				int bottomY = j;
+				int bottom = grid[bottomX][bottomY];
 
 				int leftX = i;
 				int leftY = j - n / 2;
@@ -82,13 +83,20 @@ public class GetBiggestThree { // O(m*n)
 
 				sum += bottom + top + right + left;
 
-//				sum += findBetween(grid, top, right);
-//				sum += findBetween(grid, right, bottom);
-//				sum += findBetween(grid, bottom, left);
-//				sum += findBetween(grid, left, top);
+				sum += findBetween(grid, topX, topY, rightX, rightY, 1, 1);
+				r.add(sum);
 			}
 		}
 
-		r.add(sum);
+	}
+
+	private int findBetween(int[][] grid, int startX, int startY, int stopX, int stopY, int incrementX, int incrementY) {
+		int sum = 0;
+		while (true) {
+			startX += incrementX;
+			startY += incrementY;
+			if (startX == stopX && startY == stopY) return sum;
+			sum += grid[startX][startY];
+		}
 	}
 }
