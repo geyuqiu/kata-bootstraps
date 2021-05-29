e7d70d3fc5127364cb266cc0da76a1b3e33ef58b
ge-yuqiu <yuqiu.ge@gmail.com>
2021-05-29 19:38:52 +0200

    [5757]: fix diameters and basic findDiameter from middlepoint of circle

--- java/junit5/src/main/java/leetcode/GetBiggestThree.java
+++ java/junit5/src/main/java/leetcode/GetBiggestThree.java
@@ -7,23 +7,24 @@ import java.util.*;
  */
 public class GetBiggestThree { // O(m*n)
 	List<Integer> r = new ArrayList<>();
-	int[] getBiggestThree(int[][] grid) { // each of the corners centered in a grid cell
+	int[] getBiggestThree(int[][] grid) {
 		int m = grid.length;
 		int n = grid[0].length;
 		findDiameter1x1(grid, m, n);
 
 		int index = 0;
 		if ( m >= 3 && n >= 3) {
-			int[] diameters = new int[50]; // m, n <= 100
-			for (int i = 1; i < 100; i+=2, index++) {
+			int[] diameters = new int[49]; // m, n <= 100
+			for (int i = 3; i < 100; i+=2, index++) {
 				diameters[index] = i;
 			}
-			for (int i = 0; i < 50; i++) {
+			for (int i = 0; i < 49; i++) {
 				int d = diameters[i];
 				if (d <= m && d <= n) findDiameter(grid, m, n, d);
 			}
 		}
 
+		// todo: replace with max 3 items without duplicates
 		Collections.sort(r, Collections.reverseOrder());
 		LinkedHashMap<Integer, Boolean> hm = new LinkedHashMap<>();
 		for (int i: r) {
@@ -43,6 +44,8 @@ public class GetBiggestThree { // O(m*n)
 			index++;
 			if (index == 3) break;
 		}
+
+		System.out.println(hm);
 		return res;
 	}
 
@@ -54,10 +57,14 @@ public class GetBiggestThree { // O(m*n)
 		}
 	}
 
-	private void findDiameter(int[][] grid, int i, int m, int diameter) {
+	private void findDiameter(int[][] grid, int m, int n, int diameter) { // each of the corners centered in a grid cell, 7 = 7/2 + 1 + 7/2
 		int sum = 0;
 
-
+		for (int i = m / 2; i <= m-1-m/2; i++) {
+			for (int j = n/2; j <= n -1-n/2; j++) {
+				sum += grid[i+ m/2][j] + grid[i- m/2][j] + grid[i][j+ n/2] + grid[i][j - n/2];
+			}
+		}
 
 		r.add(sum);
 	}
