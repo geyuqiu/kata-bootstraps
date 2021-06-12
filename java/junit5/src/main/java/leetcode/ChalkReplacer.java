822c9b5c76df3401e2bd7094ab379ced47e3bbd3
Yuqiu Ge <yuqiu.ge@gmail.com>
2021-06-12 16:51:24 +0200

    [5768]: use sum of array

--- java/junit5/src/main/java/leetcode/ChalkReplacer.java
+++ java/junit5/src/main/java/leetcode/ChalkReplacer.java
@@ -6,10 +6,18 @@ package leetcode;
 public class ChalkReplacer {
 	int chalkReplacer(int[] chalk, int k) { // TLE
 		int i=0;
+		int sum = 0;
+		int N = chalk.length;
+		for (int j = 0; j < N; j++) {
+			sum += chalk[j];
+		}
+
+		k = k % sum;
+
 		while (k>0) {
 			if (k < chalk[i]) return i;
 			k -= chalk[i];
-			if (i != chalk.length-1) i++;
+			if (i != N -1) i++;
 			else i=0;
 		}
 		return i;
