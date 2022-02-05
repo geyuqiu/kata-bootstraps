590e42f90ad2127ad49de70cfad928f94b3f2850
Yuqiu Ge <yuqiu.ge@gmail.com>
2022-02-05 17:13:28 +0100

    more simpler

--- java/junit5/src/main/java/leetcode/PivotArray.java
+++ java/junit5/src/main/java/leetcode/PivotArray.java
@@ -1,61 +1,23 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PivotArray {
   int[] pivotArray(int[] nums, int pivot) {
-    Item[] items = new Item[nums.length];
-    for (int i = 0; i < nums.length; i++) {
-      items[i] = new Item(nums[i], i);
+    List<Integer> smaller = new ArrayList<>();
+    List<Integer> equal = new ArrayList<>();
+    List<Integer> bigger = new ArrayList<>();
+
+    for (int n: nums) {
+      if (n < pivot) smaller.add(n);
+      else if (n == pivot) equal.add(n);
+      else bigger.add(n);
     }
 
-    Arrays.sort(items, Comparator
-      .comparing(Item::getNum));
-    int pivotStart = 0;
-    int pivotEnd = 0;
-    for (int i = 0; i < items.length; i++) {
-      if (items[i].num == pivot) {
-        pivotStart = i;
-        break;
-      }
-    }
-    for (int i = items.length-1; i >= 0; i--) {
-      if (items[i].num == pivot) {
-        pivotEnd = i;
-        break;
-      }
-    }
+    smaller.addAll(equal);
+    smaller.addAll(bigger);
 
-    Arrays.sort(items, 0, pivotStart, Comparator
-      .comparing(Item::getIndex));
-    Arrays.sort(items, pivotEnd+1, nums.length, Comparator
-      .comparing(Item::getIndex));
-
-
-    int[] res = new int[nums.length];
-    for (int i = 0; i < nums.length; i++) {
-      res[i] = items[i].num;
-    }
-
-    return res;
-  }
-}
-
-class Item{
-  int num;
-  int index;
-
-  public int getNum() {
-    return num;
-  }
-
-  public int getIndex() {
-    return index;
-  }
-
-  public Item(int num, int index) {
-    this.num = num;
-    this.index = index;
+    return smaller.stream().mapToInt(Integer::intValue).toArray();
   }
 }
