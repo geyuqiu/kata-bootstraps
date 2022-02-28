c7360620f4d8c24db2559ff5486dde486e6213e3
Yuqiu Ge <yuqiu.ge@gmail.com>
2022-02-28 20:28:11 +0100

    add 3 solution

--- java/junit5/src/main/java/codility/Solution.java
+++ java/junit5/src/main/java/codility/Solution.java
@@ -1,11 +1,29 @@
 package codility;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Solution {
-  //
+  // saved contacts: return contact with given substring
+  // if more than 1 available, return alphabetically (contact) smallest one of all
+  // return "NO CONTACT" if no match found
   public String solution(String[] A, String[] B, String P) {
-    return "a";
+    List<String> contactsFiltered = new ArrayList<>();
+
+    // check if substring
+    for (int i = 0; i < A.length; i++) {
+      if (B[i].indexOf(P) != -1) {
+        contactsFiltered.add(A[i]);
+      }
+    }
+
+    if (contactsFiltered.isEmpty()) return "NO CONTACT";
+
+    // return alphabetically (contact) smallest one of all
+    // sort string array alphabetically java
+    Collections.sort(contactsFiltered);
+
+    return contactsFiltered.get(0);
   }
 }
