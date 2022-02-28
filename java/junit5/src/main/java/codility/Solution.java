7e70f2c080a946c78fae0ecc8a8d088aba62baa1
Yuqiu Ge <yuqiu.ge@gmail.com>
2022-02-28 20:11:29 +0100

    add 2 solution

--- java/junit5/src/main/java/codility/Solution.java
+++ java/junit5/src/main/java/codility/Solution.java
@@ -6,8 +6,20 @@ import java.util.List;
 class Solution {
   // only rings that center at (0,0)
   // performance does not matter
+  // border not included
   public int solution(int inner, int outer, int[] points_x, int[] points_y) {
+    // calculate radius pythagoras (absolute values)
+    // check: inner < radius < outer
+    int N = points_x.length;
+    int result = 0;
 
-    return 1;
+    for (int i = 0; i < N; i++) {
+      double radius = Math.sqrt((points_x[i]*points_x[i])+(points_y[i]*points_y[i]));
+      if (radius > inner && radius < outer) {
+        result++;
+      }
+    }
+
+    return result;
   }
 }
