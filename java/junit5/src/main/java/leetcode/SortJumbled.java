4fbe433dc2de824632376748e725744067448419
Yuqiu Ge <yuqiu.ge@gmail.com>
2022-03-05 16:42:27 +0100

    sortJumbled

--- java/junit5/src/main/java/leetcode/SortJumbled.java
+++ java/junit5/src/main/java/leetcode/SortJumbled.java
@@ -8,22 +8,12 @@ import java.util.List;
 public class SortJumbled {
   // Elements with the same mapped values should appear in the same relative order as in the input.
   int[] sortJumbled(int[] mapping, int[] nums) {
-    List<Mapping> mappings = new ArrayList<>();
+    List<Mapping> mappings = calcMappings(mapping, nums);
 
-    // calc mapping
-    for (int i = 0; i < nums.length; i++) {
-      int number = nums[i];
-      String mapped = "";
-      while (number > 0) {
-        int remainder = number % 10;
-        number /= 10;
-        mapped = mapping[remainder-1] + mapped;
-      }
-      mappings.add(new Mapping(nums[i], Integer.parseInt(mapped)));
-    }
-
-    // sort values based on mapped
-    Collections.sort(mappings, Comparator.comparing(Mapping::getMapped));
+    Collections.sort(mappings,
+      Comparator.comparing(Mapping::getMapped)
+        .thenComparing(Mapping::getIndex)
+    );
 
     int[] results = new int[nums.length];
     for (int i = 0; i < mappings.size(); i++) {
@@ -32,15 +22,34 @@ public class SortJumbled {
 
     return results;
   }
+
+  private List<Mapping> calcMappings(int[] mapping, int[] nums) {
+    List<Mapping> mappings = new ArrayList<>();
+
+    for (int i = 0; i < nums.length; i++) {
+      int number = nums[i];
+      String mapped = "";
+      if (number == 0) mapped = mapping[0] + "";
+      while (number > 0) {
+        int remainder = number % 10;
+        number /= 10;
+        mapped = mapping[remainder] + mapped;
+      }
+      mappings.add(new Mapping(nums[i], Integer.parseInt(mapped), i));
+    }
+    return mappings;
+  }
 }
 
 class Mapping {
   int value;
   int mapped;
+  int index;
 
-  public Mapping(int value, int mapped) {
+  public Mapping(int value, int mapped, int index) {
     this.value = value;
     this.mapped = mapped;
+    this.index = index;
   }
 
   public int getMapped() {
@@ -49,5 +58,9 @@ class Mapping {
 
   public int getValue() {
     return value;
+  }
+
+  public int getIndex() {
+    return index;
   }
 }
--- java/junit5/src/test/java/leetcode/SortJumbledTest.java
+++ java/junit5/src/test/java/leetcode/SortJumbledTest.java
@@ -16,8 +16,10 @@ class SortJumbledTest {
   private static Stream<Arguments>
   sortJumbledTest() {
     return Stream.of(
-      Arguments.of(new int[]{8,9,4,0,2,1,3,5,7,6}, new int[]{991,338,38}, new int[]{338,38,991}),
-      Arguments.of(new int[]{0,1,2,3,4,5,6,7,8,9}, new int[]{789,456,123}, new int[]{123,456,789})
+      Arguments.of(new int[]{9,8,7,6,5,4,3,2,1,0}, new int[]{0,1,2,3,4,5,6,7,8,9}, new int[]{9,8,7,6,5,4,3,2,1,0}),
+      Arguments.of(new int[]{0,1,2,3,4,5,6,7,8,9}, new int[]{789,456,123}, new int[]{123,456,789}),
+      Arguments.of(new int[]{0,1,2,3,4,5,6,7,8,9}, new int[]{0,999999999}, new int[]{0,999999999}),
+      Arguments.of(new int[]{8,9,4,0,2,1,3,5,7,6}, new int[]{991,338,38}, new int[]{338,38,991})
     );
   }
   @ParameterizedTest
