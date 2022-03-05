41b83aed2e0bfc21412309c3f2738094ddf07cd3
Yuqiu Ge <yuqiu.ge@gmail.com>
2022-03-05 16:14:51 +0100

    todo: keep same relative order

--- java/junit5/src/main/java/leetcode/SortJumbled.java
+++ java/junit5/src/main/java/leetcode/SortJumbled.java
@@ -1,6 +1,8 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortJumbled {
@@ -10,16 +12,42 @@ public class SortJumbled {
 
     // calc mapping
     for (int i = 0; i < nums.length; i++) {
-
+      int number = nums[i];
+      String mapped = "";
+      while (number > 0) {
+        int remainder = number % 10;
+        number /= 10;
+        mapped = mapping[remainder-1] + mapped;
+      }
+      mappings.add(new Mapping(nums[i], Integer.parseInt(mapped)));
     }
 
     // sort values based on mapped
+    Collections.sort(mappings, Comparator.comparing(Mapping::getMapped));
 
-    return new int[]{1};
+    int[] results = new int[nums.length];
+    for (int i = 0; i < mappings.size(); i++) {
+      results[i] = mappings.get(i).getValue();
+    }
+
+    return results;
   }
 }
 
 class Mapping {
   int value;
   int mapped;
+
+  public Mapping(int value, int mapped) {
+    this.value = value;
+    this.mapped = mapped;
+  }
+
+  public int getMapped() {
+    return mapped;
+  }
+
+  public int getValue() {
+    return value;
+  }
 }
