67aadc475125810d77256403a1a42d4d34725567
Yuqiu Ge <yuqiu.ge@gmail.com>
2022-03-05 19:56:52 +0100

    tle

--- java/junit5/src/main/java/leetcode/GetAncestors.java
+++ java/junit5/src/main/java/leetcode/GetAncestors.java
@@ -10,29 +10,38 @@ import java.util.stream.Collectors;
 
 public class GetAncestors {
   // return ancestors (of ancestors) sorted in a DAG
+  Map<Integer, Set<Integer>> graph = new HashMap<>();
+  Map<Integer, Set<Integer>> allAncestors = new HashMap<>();
   List<List<Integer>> getAncestors(int n, int[][] edges) {
-    Map<Integer, Set<Integer>> graph = new HashMap<>();
     for (int i=0; i<n; i++) {
       graph.put(i, new HashSet<>());
     }
+    for (int i=0; i<n; i++) {
+      allAncestors.put(i, new HashSet<>());
+    }
     for (int i = 0; i < edges.length; i++) {
       int[] edge = edges[i];
-      int ancestor = edge[0];
-      HashSet<Integer> ancestors = (HashSet<Integer>) graph.get(ancestor);
-      graph.get(edge[1]).add(ancestor);
-      if (!ancestors.isEmpty()) {
-        for (int node: ancestors) {
-          graph.get(edge[1]).add(node);
-        }
-      }
+      graph.get(edge[1]).add(edge[0]);
     }
+    System.out.println(graph);
 
     List<List<Integer>> result = new ArrayList<>();
 
-    for (Map.Entry<Integer, Set<Integer>> entry: graph.entrySet()) {
-      result.add(entry.getValue().stream().collect(Collectors.toList()));
+    for (int i = 0; i < n; i++) {
+      dfs(i, i);
+    }
+
+    for (Map.Entry<Integer, Set<Integer>> entry: allAncestors.entrySet()) {
+      result.add(entry.getValue().stream().sorted().collect(Collectors.toList()));
     }
 
     return result;
+  }
+
+  private void dfs(int root, int node) {
+    for (int neighbor: graph.get(node)) {
+      allAncestors.get(root).add(neighbor);
+      dfs(root, neighbor);
+    }
   }
 }
